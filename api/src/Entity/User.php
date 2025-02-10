7f2e3a4c7f4bbefa1ffa11d4325b3697fe9fb70a
Alexander Matte <alex.matte71@gmail.com>
2025-02-10 19:56:40 +0100

    Added JWT auth

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -7,7 +7,6 @@ use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\Post;
-use ApiPlatform\Metadata\Put;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Delete;
 use Doctrine\Common\Collections\ArrayCollection;
