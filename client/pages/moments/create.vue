85d63a622274ad98b06037fdbb98e29b38442ac6
Alexander Matte <alex.matte71@gmail.com>
2025-02-17 18:46:47 +0100

    Added basic date picker

--- client/pages/moments/create.vue
+++ client/pages/moments/create.vue
@@ -2,8 +2,12 @@
 import { z } from 'zod';
 import type { FormSubmitEvent } from '#ui/types';
 import { momentSchema } from '@/src/interfaces/schemas/momentSchema';
+import { sub, format, isSameDay, type Duration } from 'date-fns'
 
 const countryStore = useCountryStore();
+const date = ref(new Date())
+const rangeSelected = ref({ start: sub(new Date(), { days: 14 }), end: new Date() })
+const selectedDateTab= ref(0)
 
 type Schema = z.output<typeof momentSchema>;
 
@@ -13,10 +17,30 @@ const formData = reactive<Schema>({
   date_from: undefined,
   date_to: undefined,
   exact_date: undefined,
-  region: '',  // Selected region
-  country_id: [],  // Array of selected countries
+  region: '', 
+  country_id: [],
   submoments: []
 });
+
+const ranges = [
+  { label: 'Last 7 days', duration: { days: 7 } },
+  { label: 'Last 14 days', duration: { days: 14 } },
+  { label: 'Last 30 days', duration: { days: 30 } },
+  { label: 'Last 3 months', duration: { months: 3 } },
+  { label: 'Last 6 months', duration: { months: 6 } },
+  { label: 'Last year', duration: { years: 1 } }
+]
+
+const dateSelectorItems = [
+  {
+    label: 'Date Range',
+    icon: 'i-heroicons-calendar-days'
+  },
+  {
+    label: 'Exact Date',
+    icon: 'i-heroicons-calendar'
+  }
+]
 
 // Fetch countries on mount if they are not already fetched
 onMounted(async () => {
@@ -30,6 +54,12 @@ watch(() => formData.region, () => {
   // Reset selected countries when region changes
   formData.country_id = [];
 });
+
+watch(rangeSelected, (newValue) => {
+  formData.date_from = newValue.start;
+  formData.date_to = newValue.end;
+}, { deep: true });
+
 
 // Get unique regions from countries
 const regions = computed(() => {
@@ -70,6 +100,14 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
   // Handle form submission
   console.log(event.data);
 }
+
+function isRangeSelected(duration: Duration) {
+  return isSameDay(rangeSelected.value.start, sub(new Date(), duration)) && isSameDay(rangeSelected.value.end, new Date())
+}
+
+function selectRange(duration: Duration) {
+  rangeSelected.value = { start: sub(new Date(), duration), end: new Date() }
+}
 </script>
 
 <template>
@@ -83,17 +121,50 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
         <UTextarea v-model="formData.description" />
       </UFormGroup>
 
-      <UFormGroup label="Start Date" name="date_from">
-        <UInput v-model="formData.date_from" type="date" />
-      </UFormGroup>
+      <UTabs v-model="selectedDateTab" :items="dateSelectorItems" />
 
-      <UFormGroup label="End Date" name="date_to">
-        <UInput v-model="formData.date_to" type="date" />
-      </UFormGroup>
+      <div v-if="selectedDateTab === 0" class="mt-4">
+        <UFormGroup label="When did this moment occure? (To-From)" name="date_from">
+          <UPopover :popper="{ placement: 'bottom-start' }">
+            <UButton icon="i-heroicons-calendar-days-20-solid">
+              {{ format(rangeSelected.start, 'd MMM, yyy') }} - {{ format(rangeSelected.end, 'd MMM, yyy') }}
+            </UButton>
 
-      <UFormGroup label="Exact Date" name="exact_date">
-        <UInput v-model="formData.exact_date" type="date" />
-      </UFormGroup>
+            <template #panel="{ close }">
+              <div class="flex items-center sm:divide-x divide-gray-200 dark:divide-gray-800">
+                <div class="hidden sm:flex flex-col py-4">
+                  <UButton
+                    v-for="(range, index) in ranges"
+                    :key="index"
+                    :label="range.label"
+                    color="gray"
+                    variant="ghost"
+                    class="rounded-none px-6"
+                    :class="[isRangeSelected(range.duration) ? 'bg-gray-100 dark:bg-gray-800' : 'hover:bg-gray-50 dark:hover:bg-gray-800/50']"
+                    truncate
+                    @click="selectRange(range.duration)"
+                  />
+                </div>
+
+                <DatePicker v-model="rangeSelected" @close="close" />
+              </div>
+            </template>
+          </UPopover>
+        </UFormGroup>
+      </div>
+
+      <div v-else class="mt-4">
+        <UFormGroup label="Exact Date" name="exact_date">
+          <UPopover :popper="{ placement: 'bottom-start' }">
+            <UButton icon="i-heroicons-calendar-days-20-solid" :label="format(date, 'd MMM, yyy')" />
+            <template #panel="{ close }">
+              <DatePicker v-model="formData.exact_date" is-required @close="close" />
+            </template>
+          </UPopover>
+        </UFormGroup>
+      </div>
+
+
 
       <!-- Region Dropdown -->
       <UFormGroup label="Region" name="region">
